822c3cfc826f122412aa45034a01334f5b43f4e9
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2020-07-26 12:20:47 +0100

    Run
    Signed-off-by: Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>

--- app/src/main/java/hub/programs/calcul8/MainActivity.java
+++ app/src/main/java/hub/programs/calcul8/MainActivity.java
@@ -1,8 +1,7 @@
 package hub.programs.calcul8;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
